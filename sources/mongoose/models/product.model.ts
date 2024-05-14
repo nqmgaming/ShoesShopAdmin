d781acbf872b13c8ea453e72f5cd2cf4134c8d8e
Nguyen Quang Minh <nguyenquangminh570@gmail.com>
2024-05-14 14:13:36 +0700

    update model

--- sources/mongoose/models/product.model.ts
+++ sources/mongoose/models/product.model.ts
@@ -1,29 +1,31 @@
-import { model, Schema, Types } from 'mongoose';
+import {model, Schema, Types} from 'mongoose';
 
 export interface Product {
-  name: string;
-  description: string;
-  price: number;
-  category: Types.ObjectId;
-  image: string;
-  stock: number;
-  rating: number;
-  size: Types.ObjectId;
-  createdAt: Date;
-  updatedAt: Date;
+    name: string;
+    description: string;
+    price: number;
+    category: Types.ObjectId;
+    imagePreview: string;
+    images: [string]
+    stock: number;
+    rating: number;
+    size: Types.ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
 }
 
 export const ProductSchema = new Schema<Product>({
-  name: { type: 'String', required: true },
-  description: { type: 'String', required: true },
-  price: { type: 'Number', required: true },
-  category: { type: Schema.Types.ObjectId, ref: 'Category', required: true },
-  image: { type: 'String', required: true },
-  stock: { type: 'Number', required: true },
-  rating: { type: 'Number', required: true },
-  size: { type: Schema.Types.ObjectId, ref: 'Size', required: true },
-  createdAt: { type: 'Date', default: Date.now },
-  updatedAt: { type: 'Date', default: Date.now },
+    name: {type: 'String', required: true},
+    description: {type: 'String', required: true},
+    price: {type: 'Number', required: true},
+    category: {type: Schema.Types.ObjectId, ref: 'Category', required: true},
+    imagePreview: {type: 'String', required: true},
+    images: [{type: 'String', required: true}],
+    stock: {type: 'Number', required: true},
+    rating: {type: 'Number', required: true},
+    size: {type: Schema.Types.ObjectId, ref: 'Size', required: true},
+    createdAt: {type: 'Date', default: Date.now},
+    updatedAt: {type: 'Date', default: Date.now},
 });
 
 export const ProductModel = model<Product>('Product', ProductSchema);
